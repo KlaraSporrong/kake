8a32991c8340ae394ba3281a07415253fb5f58ee
Astrid Gustafsson <astrid.e.gustafsson@gmail.com>
2017-06-03 20:45:40 +0200

    Fixing applicantspage. Design & popup

--- public/jobs/jobs.js
+++ public/jobs/jobs.js
@@ -2,5 +2,6 @@ angular.module('app')
 .controller('JobsCtrl',function(FirebaseService, $scope){
 	FirebaseService.getKeywords().then(function(keywords){
 		$scope.keywords = keywords;
+		$scope.person ='';
 	})
 })
